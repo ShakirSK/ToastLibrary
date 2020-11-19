93d824be36a729f9f7b13eb60701d5c82d8cef7f
ShakirSk <chisti920@gmail.com>
2020-11-19 23:49:52 +0530

    Initial

--- mylibrarytest/src/main/java/com/example/mylibrarytest/ToastMessage.kt
+++ mylibrarytest/src/main/java/com/example/mylibrarytest/ToastMessage.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.widget.Toast
 
 
-class ToastMessage {
-    fun s(c: Context?, message: String?) {
+object ToasterMessage {
+    fun show(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
 }
